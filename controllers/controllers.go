03d0d86960c2622dad88cfb0dd15ddf646838209
BrunoPolaski <polaskibruno03@gmail.com>
2024-03-02 16:50:56 -0300

    - Modularized project

---
+++ controllers/controllers.go
@@ -0,0 +1,11 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to the home page!")
+
+}
--- main.go
+++ main.go
@@ -2,21 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
+
+	"github.com/BrunoPolaski/api-go-rest/routes"
 )
-
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Welcome to the home page!")
-
-}
-
-func HandleRequest() {
-	http.HandleFunc("/", Home)
-	log.Fatal(http.ListenAndServe(":8000", nil))
-}
 
 func main() {
 	fmt.Println("Initialising the server...")
-	HandleRequest()
+	routes.HandleRequest()
 }
