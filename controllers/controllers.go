1f85bb3d85dcfbc81c00d967bfc85254c1efcf15
Bruno Polaski <polaskibruno03@gmail.com>
2024-03-07 19:32:58 -0300

    - Personalities

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,11 +1,17 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/BrunoPolaski/api-go-rest/models"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Welcome to the home page!")
+}
 
+func GetPersonalities(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(models.Personalities)
 }
--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 
+	"github.com/BrunoPolaski/api-go-rest/models"
 	"github.com/BrunoPolaski/api-go-rest/routes"
 )
 
 func main() {
+	models.Personalities = []models.Personality{
+		{Name: "Bruno", History: "I am a software engineer"},
+		{Name: "John", History: "I am a doctor"},
+	}
 	fmt.Println("Initialising the server...")
 	routes.HandleRequest()
 }
